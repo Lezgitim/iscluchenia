6c9e425a0843772a469164b3c14cfcb8ab4c9521
Rinad <921837@gmail.com>
2023-04-29 14:42:31 +0300

    2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,11 +9,18 @@ public class Main {
 //        дробного числа (типа float), и возвращает введенное значение.
 //        Ввод текста вместо числа не должно приводить к падению приложения,
 //        вместо этого, необходимо повторно запросить у пользователя ввод данных.);
-
 //        drobnoe();
 
 //        2. Исправить код.
-        zadanie2();
+//        zadanie2();
+
+//        3. Исправить код.
+//        zadanie3();
+//        printSum(5, 5);
+
+//        Разработайте программу, которая выбросит Exception, когда пользователь вводит
+//        пустую строку. Пользователю должно показаться сообщение, что пустые строки
+//        вводить нельзя.
 
     }
 
@@ -28,7 +35,7 @@ public class Main {
         }
 
     }
-private static void zadanie2(){
+    private static void zadanie2(){
         try {
             int d = 0;
             int[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
@@ -39,5 +46,24 @@ private static void zadanie2(){
         }
 
     }
+    private static void zadanie3(){
+            try {
+                int a = 90;
+                int b = 3;
+                System.out.println(a / b);
+                printSum(23, 234);
+                int[] abc = { 1, 2 };
+                abc[3] = 9;
+            } catch (NullPointerException ex) {
+                System.out.println("Что-то пошло не так...");
+            } catch (IndexOutOfBoundsException ex) {
+                System.out.println("Указатель не может указывать на null!");
+            } catch (Throwable ex) {
+                System.out.println("Массив выходит за пределы своего размера!");
+            }
+        }
+        public static void printSum(Integer a, Integer b) {
+            System.out.println(a + b);
+        }
 
-}
+    }
